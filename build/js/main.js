d47b4b97534725d6b2189d3d2480fdb870c14e9f
SOVANNARO <sovannaro066@gmail.com>
2024-06-17 00:19:33 +0700

    docs: change to default readme.md

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,3 @@
 "use strict";
 let username = "sovannaro";
 console.log(username);
-let a = 10;
-let b = '6';
-let c = 2;
-console.log(a + b);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,2 @@
 let username = "sovannaro";
 console.log(username);
-
-let a: number = 10;
-let b: string = '6';
-let c: number = 2;
-
-console.log(a + b);
